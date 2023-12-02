d8dd1257fa79e04e37082ab10b9d8a42fe4f211b
strayge <strayge@gmail.com>
2023-12-02 11:15:18 +0400

    add timings

--- common.py
+++ common.py
@@ -1,8 +1,28 @@
 import os
+import time
 import httpx
 from dotenv import load_dotenv
 from IPython.display import display, Markdown
 import parsel
+
+TIMES: dict[tuple[int, int, str], float] = {}
+
+
+def save_time(day: int, part: int, action: str, override: bool = False) -> None:
+    key = (day, part, action)
+    if key not in TIMES or override:
+        TIMES[(day, part, action)] = time.time()
+
+
+def show_time(day: int, part: int, action1: str, action2: str) -> str:
+    key1 = (day, part, action1)
+    key2 = (day, part, action2)
+    time1 = TIMES[key1]
+    time2 = TIMES[key2]
+    diff = time2 - time1
+    minutes = int(diff // 60)
+    seconds = int(diff % 60)
+    return f'{minutes:02}:{seconds:02}'
 
 
 def get_session() -> str:
@@ -25,6 +45,7 @@ def get_task(day: int, part: int) -> str:
     url = f'https://adventofcode.com/2023/day/{day}'
     r = httpx.get(url, cookies={'session': get_session()})
     assert r.status_code == 200, f'{r.status_code}: {r.text}'
+    save_time(day, part, 'task')
     selector = parsel.Selector(r.text)
     task = selector.xpath('//article[@class="day-desc"]').getall()
     return task[part - 1]
@@ -54,6 +75,7 @@ def send_result(day: int, part: int, answer: str) -> str:
     }
     r = httpx.post(url, cookies={'session': get_session()}, data=data)
     assert r.status_code == 200, f'{r.status_code}: {r.text}'
+    save_time(day, part, 'send', override=True)
 
     text = r.text
     text = text.split("If you're stuck, make sure you're using the full input data; there are also", 1)[0]
@@ -62,4 +84,7 @@ def send_result(day: int, part: int, answer: str) -> str:
 
     message_lines = selector.xpath('//main//article//p//text()').getall()
     message = ''.join(message_lines)
-    return message.strip()
+    message = message.strip()
+
+    full_time = show_time(day, part, 'task', 'send')
+    return message + f' (solved in {full_time})'
