01a12d2dfe2c6f93e120fa19460fea2086b7056a
strayge <strayge@gmail.com>
2025-12-02 12:00:51 +0600

    2025 days 1-2

---
+++ 2025/common.py
@@ -0,0 +1,120 @@
+import os
+import time
+import httpx
+from dotenv import load_dotenv
+from IPython.display import display, Markdown
+import parsel
+import ipynbname
+import re
+
+TIMES: dict[tuple[int, int, str], float] = {}
+YEAR = 2025
+
+
+def get_day_from_ipynb_name() -> int:
+    ipynb_name = ipynbname.name()
+    day = re.search(r'\d+', ipynb_name)
+    if day:
+        return int(day.group(0))
+    raise ValueError("Day not found in ipynb name")
+
+
+DAY = get_day_from_ipynb_name()
+
+
+def save_time(day: int, part: int, action: str, override: bool = False) -> None:
+    key = (day, part, action)
+    if key not in TIMES or override:
+        TIMES[(day, part, action)] = time.time()
+
+
+def show_time(day: int, part: int, action1: str, action2: str) -> str:
+    key1 = (day, part, action1)
+    key2 = (day, part, action2)
+    time1 = TIMES[key1]
+    time2 = TIMES[key2]
+    diff = time2 - time1
+    minutes = int(diff // 60)
+    seconds = int(diff % 60)
+    return f'{minutes:02}:{seconds:02}'
+
+
+def get_session() -> str:
+    load_dotenv()
+    return os.environ['AOC_SESSION']
+
+
+def get_input(day: int) -> str:
+    url = f'https://adventofcode.com/{YEAR}/day/{day}/input'
+    r = httpx.get(url, cookies={'session': get_session()})
+    assert r.status_code == 200, f'{r.status_code}: {r.text}'
+    return r.text
+
+
+def get_input_lines(day: int) -> list[str]:
+    return get_input(day).splitlines()
+
+
+def get_task(day: int, part: int) -> str:
+    url = f'https://adventofcode.com/{YEAR}/day/{day}'
+    r = httpx.get(url, cookies={'session': get_session()})
+    assert r.status_code == 200, f'{r.status_code}: {r.text}'
+    save_time(day, part, 'task')
+    selector = parsel.Selector(r.text)
+    task = selector.xpath('//article[@class="day-desc"]').getall()
+    return task[part - 1]
+
+
+def get_test_input(day: int, part: int = 1, pos: int | None = None) -> str:
+    task = get_task(day, part)
+    selector = parsel.Selector(task)
+    pre_values = selector.xpath('//pre//text()').getall()
+    max_length = max(len(value) for value in pre_values)
+    threshold = max(20, max_length * 0.6)
+    values = [(len(value), value) for value in pre_values if len(value) >= threshold]
+    if pos is not None:
+        return values[pos - 1][1]
+    return sorted(values, key=lambda x: x[0], reverse=True)[0][1]
+
+
+def get_test_input_lines(day: int, part: int = 1, pos: int | None = None) -> list[str]:
+    return get_test_input(day, part, pos).splitlines()
+
+
+def get_day_status(day: int) -> str:
+    url = f'https://adventofcode.com/{YEAR}/day/{day}'
+    r = httpx.get(url, cookies={'session': get_session()})
+    assert r.status_code == 200, f'{r.status_code}: {r.text}'
+    selector = parsel.Selector(r.text)
+    return selector.xpath('//main//p[@class="day-success"]//text()').get()
+
+
+def show_md(md: str) -> None:
+    display(Markdown(md))
+
+
+def show_task(day: int, part: int = 1) -> None:
+    show_md(get_task(day, part))
+
+
+def send_result(day: int, part: int, answer: str) -> str:
+    url = f'https://adventofcode.com/{YEAR}/day/{day}/answer'
+    data = {
+        'level': part,
+        'answer': answer,
+    }
+    r = httpx.post(url, cookies={'session': get_session()}, data=data)
+    assert r.status_code == 200, f'{r.status_code}: {r.text}'
+    save_time(day, part, 'send', override=True)
+
+    text = r.text
+    text = text.split("If you're stuck, make sure you're using the full input data; there are also", 1)[0]
+    text = text.split('You can <span class="share"', 1)[0]
+    selector = parsel.Selector(text)
+
+    message_lines = selector.xpath('//main//article//p//text()').getall()
+    message = ''.join(message_lines)
+    message = message.strip()
+
+    full_time = show_time(day, part, 'task', 'send')
+    return message + f' (solved in {full_time})'
---
+++ 2025/structures/map.py
@@ -0,0 +1,107 @@
+from functools import lru_cache
+from typing import Callable, Iterable, Self
+
+
+class Map:
+    def __init__(self, h: int, w: int, value: int | str = -1) -> None:
+        self.w = w
+        self.h = h
+        self.map = self.create(h, w, value)
+        self.show_remap: dict[int | str, int | str] = {-1: '.'}
+        self.get_nearest_points = lru_cache(maxsize=None)(self.get_nearest_points)
+
+    @property
+    def width(self) -> int:
+        return self.w
+
+    @property
+    def height(self) -> int:
+        return self.h
+
+    def __getitem__(self, y: int) -> list[int | str]:
+        return self.map[y]
+
+    @classmethod
+    def from_object(cls, lines: list[str]) -> Self:
+        h = len(lines)
+        w = len(lines[0])
+        map = cls(h, w)
+        for y, line in enumerate(lines):
+            for x, value in enumerate(line):
+                map.map[y][x] = value
+        return map
+
+    def create(self, y: int, x: int, value: int | str = -1) -> list[list[int | str]]:
+        line = [value] * x
+        return [line[:] for _ in range(y)]
+
+    def show(self, width: int = 2) -> None:
+        for line in self.map:
+            for value in line:
+                value = self.show_remap.get(value, value)
+                print(f'{value:>{width}} ', end='')
+            print()
+
+    def find_value(self, value: int | str) -> tuple[int, int]:
+        for y, line in enumerate(self.map):
+            for x, d in enumerate(line):
+                if d == value:
+                    return y, x
+        raise ValueError('Not found')
+
+    def normalize_point(self, y: int, x: int) -> tuple[int, int] | None:
+        if not (0 <= y < self.h) or not (0 <= x < self.w):
+            return None
+        return y, x
+
+    def get_nearest_points(self, y: int, x: int) -> list[tuple[int, int]]:
+        result = []
+        for ydiff, xdiff in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            new_coords = self.normalize_point(y + ydiff, x + xdiff)
+            if new_coords is not None:
+                result.append(new_coords)
+        return result
+
+    def get_nearest_points_by_value(self, y: int, x: int, value: int) -> Iterable[tuple[int, int, int | str]]:
+        for new_y, new_x in self.get_nearest_points(y, x):
+            new_value = self.map[new_y][new_x]
+            if new_value == value:
+                yield new_y, new_x, new_value
+
+    def calc_inc_step(self, condition: Callable | None = None) -> bool:
+        def target_filter(y, x, value, new_y, new_x, new_value):
+            if condition is not None and not condition(y, x, value, new_y, new_x, new_value):
+                return False
+            return new_value == -1 or new_value > value + 1
+
+        def target_action(y, x, value, new_y, new_x, new_value):
+            self.map[new_y][new_x] = value + 1
+
+        return self.calc_step(
+            point_filter=lambda y, x, value: value != -1,
+            target_filter=target_filter,
+            target_action=target_action,
+        )
+
+    def calc_step(
+        self,
+        point_filter: Callable | None = None,
+        target_filter: Callable | None = None,
+        target_action: Callable | None = None,
+        target_value: int | str | None = None,
+    ) -> bool:
+        any_change = False
+        for y, line in enumerate(self.map):
+            for x, value in enumerate(line):
+                if point_filter is not None and not point_filter(y, x, value):
+                    continue
+                for new_y, new_x in self.get_nearest_points(y, x):
+                    new_value = self.map[new_y][new_x]
+                    if target_filter is not None and not target_filter(y, x, value, new_y, new_x, new_value):
+                        continue
+                    if target_value is not None:
+                        self.map[new_y][new_x] = target_value
+                    if target_action is not None:
+                        target_action(y, x, value, new_y, new_x, new_value)
+                    any_change = True
+        return any_change
